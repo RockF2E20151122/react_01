e3d10e5d4506fa94116b3ef9fce15bb6eb4dc70b
rockGithub <rock20110414@gmail.com>
2015-11-22 12:42:21 +0800

    start commit

---
+++ test.js
@@ -0,0 +1,8 @@
+
+
+//for the test of exteral script file
+// <script type="text/babel" src="test.js">
+      ReactDOM.render(
+        <h1>Hello, world2!</h1>,
+        document.getElementById('Rock')
+      );
